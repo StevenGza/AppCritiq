57fe9b76fa35d3609d89796ba4c978947642573c
StevenG <stevengza@gmail.com>
2013-07-21 14:55:48 +0200

    Full Basics Complete
    Now the code begins

---
+++ AppCritiq/src/appcritiq/AppCritiq.java
@@ -0,0 +1,30 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package appcritiq;
+
+/**
+ *
+ * 1. Relate database tables
+ * 1.5 Fill database with records
+ * Code the SQL to access the database that will make up the serverside
+ * 2. Make and organise GUI for the clientside
+ * 3. Make MailServer with email for the server(is somewhere in the PAT bookmarks)
+ * 4. Work out the server side of the project with threads to handle things as well
+ * as synchronous system for access to the database
+ * 5. Speak to Sir about going through online with a project
+ * 6. 
+ * #ToDo    
+ * 
+ * @author Marcus
+ */
+public class AppCritiq {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
